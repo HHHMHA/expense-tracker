6eeafa06c146736717ff353a853a7cc18a45c319
J2mF <mp4-12cs5@hotmail.com>
2021-10-11 21:25:56 +0300

    Add Widget For Empty Transactions

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,6 +1,7 @@
 import 'package:expense_tracker/models/transaction.dart';
 import 'package:expense_tracker/widgets/transaction_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_conditional_rendering/conditional.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -11,11 +12,35 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
-        itemBuilder: (ctx, index) {
-          return TransactionWidget(transaction: transactions[index]);
-        },
-        itemCount: transactions.length,
+      child: Conditional.single(
+        context: context,
+        conditionBuilder: (context) => transactions.isNotEmpty,
+        widgetBuilder: (context) => ListView.builder(
+          itemBuilder: (ctx, index) {
+            return TransactionWidget(
+              transaction: transactions[index],
+            );
+          },
+          itemCount: transactions.length,
+        ),
+        fallbackBuilder: (context) => Column(
+          children: [
+            Text(
+              'No transactions added yet',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 200,
+              child: Image.asset(
+                'assets/images/waiting.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

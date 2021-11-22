958d95299ea03c5bd2e17d27bac1a9388c0927ae
J2mF <mp4-12cs5@hotmail.com>
2021-11-22 08:27:06 +0200

    Update Transaction Widget Style

--- lib/widgets/transaction_widget.dart
+++ lib/widgets/transaction_widget.dart
@@ -1,6 +1,5 @@
 import 'package:expense_tracker/common/constants.dart';
 import 'package:expense_tracker/models/transaction.dart';
-import 'package:expense_tracker/widgets/ui/border_box.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -14,28 +13,32 @@ class TransactionWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Row(
-        children: [
-          BorderBox(
-            child: Text(
-              '\$${transaction.amount.toStringAsFixed(Constants.defaultPrecession)}',
-              style: Theme.of(context).textTheme.headline6,
-            ),
-          ),
-          Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Text(
-              transaction.title,
-              style: TextStyle(
-                fontSize: Styles.defaultFontSize,
-                fontWeight: FontWeight.bold,
+      elevation: 5,
+      margin: EdgeInsets.symmetric(
+        vertical: 8,
+        horizontal: 5,
+      ),
+      child: ListTile(
+        leading: CircleAvatar(
+          radius: 30,
+          child: Padding(
+            padding: const EdgeInsets.all(6.0),
+            child: FittedBox(
+              child: Text(
+                '\$${transaction.amount.toStringAsFixed(Constants.defaultPrecession)}',
               ),
             ),
-            Text(
-              DateFormat.yMMMd().format(transaction.date),
-              style: TextStyle(color: Colors.grey),
-            ),
-          ]),
-        ],
+          ),
+        ),
+        title: Text(
+          transaction.title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        subtitle: Text(
+          DateFormat.yMMMd().format(
+            transaction.date,
+          ),
+        ),
       ),
     );
   }
